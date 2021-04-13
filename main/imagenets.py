7612be2709d3fbb0ea941720ae2d5d27a1073106
Matheus B <matheus_ob59@hotmail.com>
2021-04-12 22:21:09 -0300

    Update for remote testing.

--- main/imagenets.py
+++ main/imagenets.py
@@ -13,6 +13,7 @@ labels_path = '../src/labels/imagenet_classes.txt'
 ground_truth_path = '../src/groundtruth/ILSVRC2012_val.txt'
 # imgname = '../src/imagenet2012_obj/ILSVRC2012_val_00000004.JPEG'
 image_src = os.listdir(img_src_path)
+image_src.sort()
 data_set_size = 25
 
 # Load MobileNetV2
